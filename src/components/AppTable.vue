ed4dbb52d6f205894c198b566e7e4f302f86fa22
erkinov97 <erkinov997@gmail.com>
2023-01-24 22:31:56 +0500

    added sort function

--- src/components/AppTable.vue
+++ src/components/AppTable.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { ref } from "vue";
+
 const props = defineProps({
   comments: {
     type: [Object, Array],
@@ -18,32 +20,49 @@ const emit = defineEmits(["open-modal"]);
 function openModal(id) {
   emit("open-modal", id);
 }
+const sortedbyASC = ref(true);
+
+function sortList(sortBy) {
+  if (sortedbyASC.value) {
+    // eslint-disable-next-line vue/no-mutating-props
+    props.comments.sort((a, b) => (a[sortBy] > b[sortBy] ? -1 : 1));
+    sortedbyASC.value = false;
+  } else {
+    // eslint-disable-next-line vue/no-mutating-props
+    props.comments.sort((a, b) => (a[sortBy] < b[sortBy] ? -1 : 1));
+    sortedbyASC.value = true;
+  }
+}
 </script>
 
 <template>
   <table class="table">
-    <tr>
-      <th>ID</th>
-      <th>Name</th>
-      <th>Email</th>
-      <th>Body</th>
-      <th>Actions</th>
-    </tr>
-    <template v-if="props.loading">
+    <thead>
       <tr>
-        <td colspan="5" class="loading">...Load comments ...</td>
+        <th>ID</th>
+        <th @click="sortList('name')">Name &#8597;</th>
+        <th>Email</th>
+        <th @click="sortList('body')">Body &#8597;</th>
+        <th>Actions</th>
       </tr>
-    </template>
-    <template v-else>
-      <tr v-for="post in props.comments" :key="post.id">
-        <td>{{ post.id }}</td>
-        <td>{{ post.name }}</td>
-        <td>{{ post.email }}</td>
-        <td>{{ post.body }}</td>
-        <td class="button">
-          <button class="modal-btn" @click="openModal(post.id)">Open</button>
-        </td>
-      </tr>
-    </template>
+    </thead>
+    <tbody>
+      <template v-if="props.loading">
+        <tr>
+          <td colspan="5" class="loading">...Load comments ...</td>
+        </tr>
+      </template>
+      <template v-else>
+        <tr v-for="post in props.comments" :key="post.id">
+          <td>{{ post.id }}</td>
+          <td>{{ post.name }}</td>
+          <td>{{ post.email }}</td>
+          <td>{{ post.body }}</td>
+          <td class="button">
+            <button class="modal-btn" @click="openModal(post.id)">Open</button>
+          </td>
+        </tr>
+      </template>
+    </tbody>
   </table>
 </template>
